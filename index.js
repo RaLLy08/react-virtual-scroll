dc403544989a63c484ed73d69d97e476ae2b70e1
azad.k <uytemataaero@mail.ru>
2023-09-01 23:42:48 +0400

    init

---
+++ index.js
@@ -0,0 +1,158 @@
+const Placeholder = ({height}) => {
+    return <div style={{height: `${height}px`}}></div>
+}
+
+class ScrollHanlder {
+    containerHeight = null;
+    itemHeight = null;
+    itemsOnScreen = null;
+    itemsLength = null;
+
+    fromIndex = 0;
+    toIndex = 0;
+
+    fromIndexWithMargin = 0;
+    toIndexWithMargin = 0;
+
+    marginFromIndex = 0;
+    marginToIndex = 0;
+
+    scrollTop = null;
+    topPlaceholderHeight = null;
+    bottomPlaceholderHeight = null;
+    marginElementsLimit = 10;
+
+
+    constructor(containerHeight, itemHeight, itemsLength) {
+        this.containerHeight = containerHeight;
+        this.itemHeight = itemHeight;
+        this.itemsLength = itemsLength;
+
+        this.#setItemsOnScreen();
+        this.#setIndexBoundaries();
+        this.#setPlaceholdersHeights();
+    }
+
+    #setItemsOnScreen = () => {
+        this.itemsOnScreen = Math.ceil(this.containerHeight / this.itemHeight);
+    }
+
+    #setIndexBoundaries = () => {
+        this.fromIndex = Math.floor((this.scrollTop) / this.itemHeight);
+        this.toIndex = this.fromIndex + this.itemsOnScreen;
+    }
+    
+    #setIndexBoundariesWithMargin = () => {
+        this.fromIndexWithMargin = this.fromIndex - this.marginFromIndex ;
+        this.toIndexWithMargin = this.toIndex + this.marginToIndex;
+    }
+
+    #setIndexMargins = () => {
+
+        const margin = this.marginElementsLimit;
+
+        const hasPlaceTop = this.fromIndex > margin;
+        const hasPlaceBottom = this.toIndex <= this.itemsLength - margin;
+
+
+        if (hasPlaceTop) {
+            this.marginFromIndex = margin;
+        } else {
+            this.marginFromIndex = 0;
+        }
+
+        if (hasPlaceBottom) {
+            this.marginToIndex = margin;
+        } else {
+            this.marginToIndex = 0;
+        }
+
+    }
+
+    #setPlaceholdersHeights = () => {
+        this.topPlaceholderHeight = (this.fromIndexWithMargin) * this.itemHeight;
+        this.bottomPlaceholderHeight = (this.itemsLength - this.toIndexWithMargin) * this.itemHeight;
+    }
+
+    getPlaceholdersHeight = () => {
+        return [this.topPlaceholderHeight, this.bottomPlaceholderHeight];
+    }
+
+    getElementsSliceOnScreen = (elements) => {
+
+        return elements.slice(
+            this.fromIndexWithMargin,
+            this.toIndexWithMargin
+        );
+    }
+
+    onScroll = (scrollTop) => {
+
+        this.scrollTop = scrollTop;
+
+        this.#setIndexBoundaries();
+        this.#setIndexMargins()
+        this.#setIndexBoundariesWithMargin();
+
+        this.#setPlaceholdersHeights();
+    }
+}
+    
+
+function List() {
+    const containerHeight = 300;
+    const itemHeight = 15;
+
+    const els = React.useMemo(() => Array.from({ length: 10000 }, (_, i) => 
+        i
+    ), [])
+    const [scrollTop, setScrollTop] = React.useState(0);
+
+    const scrollHandler = React.useMemo(() => {
+        return new ScrollHanlder(containerHeight, itemHeight, els.length);
+    }, [containerHeight, itemHeight, els.length]);
+
+
+    const handleScroll = (e) => {
+        const { scrollTop } = e.target;
+        setScrollTop(scrollTop);
+    }
+
+    scrollHandler.onScroll(scrollTop);
+
+    const elementsOnScreen = scrollHandler.getElementsSliceOnScreen(els);
+
+    const [topPlaceholderHeight, bottomPlaceholderHeight] = scrollHandler.getPlaceholdersHeight();
+
+    
+    return <div 
+        style={{
+            height: `${containerHeight}px`, 
+            overflow: "auto",
+            margin: 'auto',
+            width: '500px',
+        }}
+        onScroll={handleScroll}
+    >   
+        <Placeholder height={topPlaceholderHeight}/>
+        {elementsOnScreen.map((el) => {
+            return <div
+                style={{height: `${itemHeight}px`}}
+                key={el}
+            >
+                Hello {el}
+            </div>;
+        })}
+        <Placeholder height={bottomPlaceholderHeight}/>
+    </div>
+}
+
+function MyApp() {
+    return <List/>
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+
+root.render(<MyApp />);
